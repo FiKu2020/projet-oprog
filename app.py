5da4dc4631c1ab25fd17328feee8784688592523
filip k <filip2006kujawa@gmail.com>
2023-11-30 19:11:56 +0100

    get users by id?

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import Response
 from flask import jsonify
 from flask import request
 
-
-
 app = Flask(__name__)
 users = [
     {"id": 1, "name": "Wojciech", "lastname": "Oczkowski"}
-    ]
+]
+
+
 @app.get('/users')
 def get_users():
     return jsonify(users), 200
+
+@app.
+def get_users_by_id():
+    if users.id in users:
+        return jsonify(users), 200
 
 print(users)
