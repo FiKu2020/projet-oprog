74eaaa4c337f91a9c6b6401331a3e13be675f61e
filip k <filip2006kujawa@gmail.com>
2023-12-01 22:01:23 +0100

    poddaje sie

--- test_app.py
+++ test_app.py
@@ -13,3 +13,14 @@ def test_get_users(client):
     assert response.status_code == 200
     data = json.loads(response.data)
     assert isinstance(data, list)
+
+def test_create_temp_users(client):
+    new_user = {
+        'name': 'John',
+        'lastname': 'Doe'
+    }
+    response = client.post('/users', json=new_user)
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+    assert 'id' in data
