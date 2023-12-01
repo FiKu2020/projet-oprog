c3a73f4f7face0dfd85e79243fc2bf077dfc00f9
filip k <filip2006kujawa@gmail.com>
2023-12-01 21:37:06 +0100

    ????????????????

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import json
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_get_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
